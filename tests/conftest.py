dffb13bc925a0897dd8ba5141f2d1d2143a08f7d
phi <phi.friday@gmail.com>
2023-07-31 01:20:42 +0900

    fix: add uvloop test

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ import pytest
 @pytest.fixture(
     params=[
         pytest.param(("asyncio", {"use_uvloop": False}), id="asyncio"),
+        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio-uvloop"),
         pytest.param(
             ("trio", {"restrict_keyboard_interrupt_to_checkpoints": True}),
             id="trio",
