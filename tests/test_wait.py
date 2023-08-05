3a709ee11847e7610bb68d224dae07b2f142ad94
phi <phi.friday@gmail.com>
2023-08-05 23:06:22 +0900

    fix: add tic in tests

--- tests/test_wait.py
+++ tests/test_wait.py
@@ -10,6 +10,8 @@ from async_wrapper import Completed, Waiter, wait_for
 from async_wrapper.exception import PendingError
 from async_wrapper.task_group import SoonValue
 
+TIC = 0.01
+
 
 @pytest.mark.anyio()
 async def test_waiter():
@@ -90,10 +92,10 @@ async def test_wait_many():
 async def test_completed_with_task_group():
     result: list[int] = []
     async with anyio.create_task_group() as task_group:
-        task_group.start_soon(sample_func2, 1, 0.3, result)
+        task_group.start_soon(sample_func2, 1, 3, result)
         async with Completed(task_group) as completed:
-            completed.start_soon(None, sample_func2, 2, 0.2)
-            completed.start_soon(None, sample_func2, 3, 0.1)
+            completed.start_soon(None, sample_func2, 2, 2)
+            completed.start_soon(None, sample_func2, 3, 1)
 
             result.extend([value async for value in completed])
 
@@ -104,10 +106,10 @@ async def test_completed_with_task_group():
 async def test_completed_without_task_group():
     result: list[int] = []
     async with anyio.create_task_group() as task_group:
-        task_group.start_soon(sample_func2, 1, 0.3, result)
+        task_group.start_soon(sample_func2, 1, 3, result)
         async with Completed() as completed:
-            completed.start_soon(task_group, sample_func2, 2, 0.2)
-            completed.start_soon(task_group, sample_func2, 3, 0.1)
+            completed.start_soon(task_group, sample_func2, 2, 2)
+            completed.start_soon(task_group, sample_func2, 3, 1)
 
             result.extend([value async for value in completed])
 
@@ -118,10 +120,10 @@ async def test_completed_without_task_group():
 async def test_completed_overwrite_task_group():
     result: list[int] = []
     async with anyio.create_task_group() as task_group:
-        task_group.start_soon(sample_func2, 1, 0.3, result)
+        task_group.start_soon(sample_func2, 1, 3, result)
         async with Completed(task_group) as completed:
-            completed.start_soon(task_group, sample_func2, 2, 0.2)
-            completed.start_soon(task_group, sample_func2, 3, 0.1)
+            completed.start_soon(task_group, sample_func2, 2, 2)
+            completed.start_soon(task_group, sample_func2, 3, 1)
 
             result.extend([value async for value in completed])
 
@@ -134,7 +136,7 @@ async def test_completed_overwrite_diff_task_group():
         async with Completed(task_group) as completed:
             async with anyio.create_task_group() as new_tg:
                 with pytest.raises(ValueError, match="diff task groups"):
-                    completed.start_soon(new_tg, sample_func2, 2, 0.2)
+                    completed.start_soon(new_tg, sample_func2, 2, 2)
 
 
 @pytest.mark.anyio()
@@ -142,7 +144,7 @@ async def test_completed_no_task_group():
     async with anyio.create_task_group():
         async with Completed() as completed:
             with pytest.raises(ValueError, match="there is no task group"):
-                completed.start_soon(None, sample_func2, 2, 0.2)
+                completed.start_soon(None, sample_func2, 2, 2)
 
 
 @pytest.mark.anyio()
@@ -150,14 +152,14 @@ async def test_completed_without_enter():
     async with anyio.create_task_group() as task_group:
         completed = Completed(task_group)
         with pytest.raises(PendingError, match="enter first"):
-            completed.start_soon(None, sample_func2, 2, 0.2)
+            completed.start_soon(None, sample_func2, 2, 2)
 
 
 @pytest.mark.anyio()
 async def test_completed_after_exit():
     async with anyio.create_task_group() as task_group:
         async with Completed(task_group) as completed:
-            completed.start_soon(None, sample_func2, 1, 0.1)
+            completed.start_soon(None, sample_func2, 1, 1)
         with pytest.raises(PendingError, match="enter first"):
             _ = [value async for value in completed]
 
@@ -177,10 +179,10 @@ async def sample_func(value: Any = None, soon: SoonValue[Any] | None = None) -> 
 
 async def sample_func2(
     value: int,
-    sleep: float,
+    sleep: int,
     result: list[int] | None = None,
 ) -> int:
-    await anyio.sleep(sleep)
+    await anyio.sleep(TIC * sleep)
     if result is not None:
         result.append(value)
     return value
