1885013e9130ca256f5b066fbb33853f2086b02e
phi <phi.friday@gmail.com>
2023-08-06 11:35:29 +0900

    fix: remove convert.abc

--- tests/convert/base.py
+++ tests/convert/base.py
@@ -3,20 +3,19 @@ from __future__ import annotations
 from typing import Final
 
 from async_wrapper import async_to_sync, sync_to_async, toggle_func
-from async_wrapper.convert.abc import AsyncToSync, SyncToAsync, Toggle
 
 
 class BaseTest:
     epsilon: Final[float] = 0.1
 
     @classmethod
-    def sync_to_async(cls) -> SyncToAsync:
+    def sync_to_async(cls):  # noqa: ANN206
         return sync_to_async
 
     @classmethod
-    def async_to_sync(cls) -> AsyncToSync:
+    def async_to_sync(cls):  # noqa: ANN206
         return async_to_sync
 
     @classmethod
-    def toggle(cls) -> Toggle:
+    def toggle(cls):  # noqa: ANN206
         return toggle_func
